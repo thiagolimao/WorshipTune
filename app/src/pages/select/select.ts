2734276a8c1bcb0dc4162e00a8d8377af7154716
Wlademyr Mendes <wlademyr.mendes@gmail.com>
2017-04-22 18:06:39 -0300

    Comentando Device

--- app/src/pages/select/select.ts
+++ app/src/pages/select/select.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
-import { Platform } from 'ionic-angular';
-import {AlertController} from 'ionic-angular';
-import { Insomnia } from '@ionic-native/insomnia';
+// import { Geolocation } from '@ionic-native/geolocation';
+// import { Platform } from 'ionic-angular';
+// import {AlertController} from 'ionic-angular';
+// import { Insomnia } from '@ionic-native/insomnia';
 
 
 @Component({
@@ -11,32 +11,32 @@ import { Insomnia } from '@ionic-native/insomnia';
   templateUrl: 'select.html'
 })
 export class SelectPage {
-
-  constructor(public navCtrl: NavController, private platform: Platform, private geolocation: Geolocation, private alertCtrl: AlertController, private insomnia: Insomnia) {
-      platform.ready().then(() => {
-        this.insomnia.keepAwake()
-        .then(
-          () => console.log('success'),
-          () => console.log('error')
-        );
-        // get current position
-        geolocation.getCurrentPosition().then(pos => {
-          let alert = this.alertCtrl.create({
-            title: 'Low battery',
-            subTitle: 'lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude,
-            buttons: ['Dismiss']
-          });
-          alert.present();
-        });
-
-        // const watch = geolocation.watchPosition().subscribe(pos => {
-        //   console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
-        // });
-
-        // to stop watching
-        // watch.unsubscribe();
-
-      });
+  // constructor(public navCtrl: NavController, private platform: Platform, private geolocation: Geolocation, private alertCtrl: AlertController, private insomnia: Insomnia) {
+  constructor(public navCtrl: NavController) {
+      // platform.ready().then(() => {
+      //   this.insomnia.keepAwake()
+      //   .then(
+      //     () => console.log('success'),
+      //     () => console.log('error')
+      //   );
+      //   // get current position
+      //   geolocation.getCurrentPosition().then(pos => {
+      //     let alert = this.alertCtrl.create({
+      //       title: 'Low battery',
+      //       subTitle: 'lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude,
+      //       buttons: ['Dismiss']
+      //     });
+      //     alert.present();
+      //   });
+      //
+      //   // const watch = geolocation.watchPosition().subscribe(pos => {
+      //   //   console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      //   // });
+      //
+      //   // to stop watching
+      //   // watch.unsubscribe();
+      //
+      // });
 
     }
 
