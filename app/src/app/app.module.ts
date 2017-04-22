70b52ead75ebf17a5d6f3fd6d3ed8b6c44701b86
Thiago Limão <thiago@thiagolima.com>
2017-04-22 02:54:13 -0300

    Base app

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp }      from './app.component';
+//import { HomePage }   from '../pages/home/home';
+//import { ListPage }   from '../pages/list/list';
+
+import { FindPage }       from '../pages/find/find';
+import { SelectPage }     from '../pages/select/select';
+import { PlaylistPage }   from '../pages/playlist/playlist';
+import { PlayPage }       from '../pages/play/play';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    //HomePage,
+    //ListPage,
+
+    FindPage,
+    SelectPage,
+    PlaylistPage,
+    PlayPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    //HomePage,
+    //ListPage,
+
+    FindPage,
+    SelectPage,
+    PlaylistPage,
+    PlayPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ app/src/pages/play/play.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-play',
+  templateUrl: 'play.html'
+})
+export class PlayPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
