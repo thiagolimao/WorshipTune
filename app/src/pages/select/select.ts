42602bba7be9c8000b1c46b9789316b5d633eca5
Thiago Limão <thiago@thiagolima.com>
2017-05-28 00:25:25 -0300

    Ajustes Wlad

--- app/src/pages/select/select.ts
+++ app/src/pages/select/select.ts
@@ -15,10 +15,14 @@ declare let cordova: any;
 export class SelectPage {
 
   songs: any;
+  activeSongs: Array<any>;
   playlist: any;
 
   constructor(public navCtrl: NavController, private platform: Platform, private geolocation: Geolocation, private alertCtrl: AlertController, private insomnia: Insomnia, private file: File) {
+      this.activeSongs =  [];
       this.playlist = 'principal';
+      this.activeSongs['principal'] = [];
+      this.activeSongs['instrumental'] = [];
 
       platform.ready().then(() => {
         this.insomnia.keepAwake()
@@ -46,6 +50,50 @@ export class SelectPage {
                 }
             );
         });
+    }
+
+    setActive(obj) {
+
+      var index = this.activeSongs[this.playlist].indexOf(obj);
+      if(index > -1)
+        this.activeSongs[this.playlist].splice(index, 1);
+      else
+        this.activeSongs[this.playlist].push(obj);
+      // console.log(this.activeSongs);
+    }
+
+    activeArray(obj) {
+      // console.log(this.activeSongs.indexOf(obj) > -1);
+      return this.activeSongs[this.playlist].indexOf(obj) > -1;
+      // return true;
+    }
+
+    playSong(obj) {
+      cordova.plugins.Music.playSong(
+          //id of song
+          obj.id,
+          // onComplete callback
+          function (msg) {
+              console.log("audio completed");
+          },
+          // error callback
+          function (e) {
+              console.log("Error getting message=" + e);
+          }
+      );
+    }
+
+    stopSong(){
+      cordova.plugins.Music.stopSong(
+          // success callback
+          function (msg) {
+              console.log("audio stopped");
+          },
+          // error callback
+          function (e) {
+              console.log("Error getting message=" + e);
+          }
+      );
     }
 
   }
